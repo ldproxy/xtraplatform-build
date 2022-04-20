f4241b31c0ad502cbe9a5ff1060fcdaca2f2f403
azahnen <zahnen@interactive-instruments.de>
2022-04-20 11:04:26 +0200

    rename custom tags

--- xtraplatform-modules/src/main/groovy/de/interactive_instruments/xtraplatform/docs/ElementDocs.java
+++ xtraplatform-modules/src/main/groovy/de/interactive_instruments/xtraplatform/docs/ElementDocs.java
@@ -11,8 +11,8 @@ import java.util.stream.Stream;
 
 class ElementDocs {
 
-  static final String LANG_PREFIX = "lang_";
-  static final String LANG_ALL = "lang_all";
+  static final String LANG_PREFIX = "lang";
+  static final String LANG_ALL = "langAll";
   static final String BODY = "body";
   static final String PROPERTY_TABLE = "propertyTable";
   static final String EXAMPLE = "example";
@@ -66,7 +66,7 @@ class ElementDocs {
     return doc.stream()
         .flatMap(map -> map.keySet().stream())
         .filter(key -> key.startsWith(LANG_PREFIX) && !key.startsWith(LANG_ALL))
-        .map(key -> key.replace(LANG_PREFIX, ""))
+        .map(key -> key.replace(LANG_PREFIX, "").toLowerCase())
         .collect(Collectors.toSet());
   }
 
@@ -77,7 +77,7 @@ class ElementDocs {
 
     String body = doc.stream()
         .flatMap(map -> map.entrySet().stream())
-        .filter(entry -> entry.getKey().startsWith(LANG_PREFIX + language)
+        .filter(entry -> entry.getKey().toLowerCase().startsWith(LANG_PREFIX + language)
             || Objects.equals(entry.getKey(), LANG_ALL)
             || Objects.equals(entry.getKey(), BODY))
         .flatMap(entry -> entry.getValue().stream())
