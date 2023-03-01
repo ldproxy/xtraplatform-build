13d1d9773d6910fce93d5ec2f81bb724db7aeb83
Andreas Zahnen <zahnen@interactive-instruments.de>
2023-03-01 08:57:58 +0100

    detect interfaces recursively

--- xtraplatform-modules/src/main/groovy/de/interactive_instruments/xtraplatform/docs/TypeScanner.java
+++ xtraplatform-modules/src/main/groovy/de/interactive_instruments/xtraplatform/docs/TypeScanner.java
@@ -2,30 +2,18 @@ package de.interactive_instruments.xtraplatform.docs;
 
 import com.sun.source.doctree.DocCommentTree;
 import com.sun.source.util.DocTrees;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.TypeParameterElement;
-import javax.lang.model.element.VariableElement;
+
+import javax.lang.model.element.*;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementScanner9;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-/**
- * A scanner to display the structure of a series of elements and their documentation comments.
- */
+/** A scanner to display the structure of a series of elements and their documentation comments. */
 class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
-
 
   private final DocTrees treeUtils;
   private final Elements elementUtils;
@@ -49,26 +37,24 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
 
   @Override
   public List<ElementDocs> visitType(TypeElement e, Integer integer) {
-    //TODO: ancestors of superClass and interfaces
     if (integer == 1) {
       TypeDocs c = new TypeDocs();
 
       c.qualifiedName = e.getQualifiedName().toString();
-      //c.name = e.getSimpleName().toString();
-      c.modifiers = e.getModifiers()
-          .stream()
-          .map(m -> m.toString())
-          .collect(Collectors.toSet());
-      c.types = e.getTypeParameters()
-          .stream()
-          .map(typeParameterElement -> typeParameterElement.toString()
-              + typeParameterElement.getBounds().toString())//TODO
-          .collect(Collectors.toList());
+      // c.name = e.getSimpleName().toString();
+      c.modifiers = e.getModifiers().stream().map(m -> m.toString()).collect(Collectors.toSet());
+      c.types =
+          e.getTypeParameters().stream()
+              .map(
+                  typeParameterElement ->
+                      typeParameterElement.toString()
+                          + typeParameterElement.getBounds().toString()) // TODO
+              .collect(Collectors.toList());
       c.superClass = scanElementRef(e.getSuperclass());
-      c.interfaces = e.getInterfaces()
-          .stream()
-          .map(typeMirror -> scanElementRef(typeMirror))
-          .collect(Collectors.toList());
+      c.interfaces =
+          getAllInterfaces(e).stream()
+              .map(typeMirror -> scanElementRef(typeMirror))
+              .collect(Collectors.toList());
       c.doc = scanDocComment(e, c.qualifiedName);
       c.annotations = scanAnnotations(e);
 
@@ -81,11 +67,29 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
     return DEFAULT_VALUE;
   }
 
+  private List<TypeMirror> getAllInterfaces(Element e) {
+    if (!(e instanceof TypeElement)) {
+      return List.of();
+    }
+    return Stream.concat(
+            Objects.nonNull(((TypeElement) e).getSuperclass())
+                ? getAllInterfaces(typeUtils.asElement(((TypeElement) e).getSuperclass())).stream()
+                : Stream.empty(),
+            ((TypeElement) e)
+                .getInterfaces().stream()
+                    .flatMap(
+                        typeMirror ->
+                            Stream.concat(
+                                Stream.of(typeMirror),
+                                getAllInterfaces(typeUtils.asElement(typeMirror)).stream())))
+        .collect(Collectors.toList());
+  }
+
   private boolean shouldInclude(TypeDocs typeDocs) {
     if (typeDocs.hasAnnotation("dagger.internal.DaggerGenerated")
-        //TODO: Generated in dagger-auto
+        // TODO: Generated in dagger-auto
         || typeDocs.getName().startsWith("AutoBindings")) {
-      //System.out.println("EXCLUDE " + typeDocs.qualifiedName);
+      // System.out.println("EXCLUDE " + typeDocs.qualifiedName);
       return false;
     }
     return true;
@@ -98,25 +102,26 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
       MethodDocs m = new MethodDocs();
       m.qualifiedName = e.getSimpleName().toString();
       m.isConstructor = e.getKind() == ElementKind.CONSTRUCTOR;
-      m.modifiers = e.getModifiers()
-          .stream()
-          .map(Modifier::toString)
-          .collect(Collectors.toSet());
-      m.types = e.getTypeParameters()
-          .stream()
-          .map(typeParameterElement -> typeParameterElement.toString()
-              + typeParameterElement.getBounds().toString())//TODO
-          .collect(Collectors.toList());
+      m.modifiers = e.getModifiers().stream().map(Modifier::toString).collect(Collectors.toSet());
+      m.types =
+          e.getTypeParameters().stream()
+              .map(
+                  typeParameterElement ->
+                      typeParameterElement.toString()
+                          + typeParameterElement.getBounds().toString()) // TODO
+              .collect(Collectors.toList());
       m.annotations = scanAnnotations(e);
-      m.doc = scanDocComment(e,typeDocs.qualifiedName + "::" + m.qualifiedName);
-      //TODO: always add for JsonProperty?
-      if (/*m.doc.stream().flatMap(d -> d.keySet().stream()).allMatch(k -> k.equals("return"))
-          &&*/ m.hasAnnotation(StepResolver.JSON_PROPERTY)
-          /*&& typeDocs.hasInterfaces()*/) {
-        List<String> parentDocs = findParentDocs(e, ((TypeElement) e.getEnclosingElement()).getInterfaces());
+      m.doc = scanDocComment(e, typeDocs.qualifiedName + "::" + m.qualifiedName);
+      // TODO: always add for JsonProperty?
+      if (
+      /*m.doc.stream().flatMap(d -> d.keySet().stream()).allMatch(k -> k.equals("return"))
+      &&*/ m.hasAnnotation(StepResolver.JSON_PROPERTY)
+      /*&& typeDocs.hasInterfaces()*/ ) {
+        List<String> parentDocs =
+            findParentDocs(e, ((TypeElement) e.getEnclosingElement()).getInterfaces());
         m.doc.add(Map.of("_overrides_", parentDocs));
       }
-      //TODO: exceptions, returnType
+      // TODO: exceptions, returnType
       m.returnType = e.getReturnType().toString();
 
       if (Objects.isNull(typeDocs.methods)) {
@@ -126,7 +131,7 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
 
       super.visitExecutable(e, integer);
     }
-    //return super.visitExecutable(e, integer);
+    // return super.visitExecutable(e, integer);
     return DEFAULT_VALUE;
   }
 
@@ -140,6 +145,7 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
     }
     return p;
   }
+
   private List<String> findParentDocs(ExecutableElement e, TypeElement parent) {
     List<String> p = new ArrayList<>();
     for (Element element : parent.getEnclosedElements()) {
@@ -161,10 +167,7 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
     TypeDocs typeDocs = getCurrentTypeDocs();
     VariableDocs v = new VariableDocs();
     v.qualifiedName = e.getSimpleName().toString();
-    v.modifiers = e.getModifiers()
-        .stream()
-        .map(Modifier::toString)
-        .collect(Collectors.toSet());
+    v.modifiers = e.getModifiers().stream().map(Modifier::toString).collect(Collectors.toSet());
     v.doc = scanDocComment(e, typeDocs.qualifiedName);
     v.annotations = scanAnnotations(e);
     v.type = e.asType().toString();
@@ -183,32 +186,36 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
         methodDocs.parameters.add(v);
       }
     } else {
-      //TODO: enums
-      //System.out.println("UNHANDLED VARIABLE " + e.getKind() + e.getSimpleName());
+      // TODO: enums
+      // System.out.println("UNHANDLED VARIABLE " + e.getKind() + e.getSimpleName());
     }
 
     return null;
   }
 
-  //TODO
+  // TODO
   @Override
   public List<ElementDocs> visitTypeParameter(TypeParameterElement e, Integer integer) {
     return super.visitTypeParameter(e, integer);
   }
 
   private List<AnnotationDocs> scanAnnotations(Element e) {
-    return e.getAnnotationMirrors()
-        .stream()
-        .map(annotationMirror -> {
-          AnnotationDocs a = new AnnotationDocs();
-          a.qualifiedName = annotationMirror.getAnnotationType().toString();
-          //a.name = annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
-          a.attributes = new LinkedHashMap<>();
-          annotationMirror.getElementValues().forEach((k, v) -> {
-            a.attributes.put(k.getSimpleName().toString(), v.getValue().toString());
-          });
-          return a;
-        })
+    return e.getAnnotationMirrors().stream()
+        .map(
+            annotationMirror -> {
+              AnnotationDocs a = new AnnotationDocs();
+              a.qualifiedName = annotationMirror.getAnnotationType().toString();
+              // a.name =
+              // annotationMirror.getAnnotationType().asElement().getSimpleName().toString();
+              a.attributes = new LinkedHashMap<>();
+              annotationMirror
+                  .getElementValues()
+                  .forEach(
+                      (k, v) -> {
+                        a.attributes.put(k.getSimpleName().toString(), v.getValue().toString());
+                      });
+              return a;
+            })
         .collect(Collectors.toList());
   }
 
@@ -217,7 +224,8 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
     DocCommentTree docCommentTree = treeUtils.getDocCommentTree(e);
 
     if (docCommentTree != null) {
-      new DocCommentScanner(elementUtils, enclosingElement, DEFAULT_VALUE, tags).visit(docCommentTree, null);
+      new DocCommentScanner(elementUtils, enclosingElement, DEFAULT_VALUE, tags)
+          .visit(docCommentTree, null);
     }
 
     return tags;
@@ -232,7 +240,7 @@ class TypeScanner extends ElementScanner9<List<ElementDocs>, Integer> {
 
     ElementDocs ref = new ElementDocs();
     ref.qualifiedName = e.getQualifiedName().toString();
-    //ref.name = e.getSimpleName().toString();
+    // ref.name = e.getSimpleName().toString();
 
     return ref;
   }
