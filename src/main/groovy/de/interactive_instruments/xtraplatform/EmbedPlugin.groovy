60fed17a01978674c792cb9648dc0465b8659f62
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-11-29 16:01:08 +0100

    use catalog instead of bom/platform

--- src/main/groovy/de/interactive_instruments/xtraplatform/EmbedPlugin.groovy
+++ src/main/groovy/de/interactive_instruments/xtraplatform/EmbedPlugin.groovy
@@ -126,7 +126,7 @@ class EmbedPlugin implements Plugin<Project> {
                 it.attributes {
                     it.attribute(Category.CATEGORY_ATTRIBUTE, project.objects.named(Category.class, Category.LIBRARY))
                     it.attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling.class, Bundling.EXTERNAL))
-                    it.attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.class, Usage.JAVA_API))
+                    it.attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.class, Usage.JAVA_RUNTIME))
                     it.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, project.objects.named(LibraryElements.class, LibraryElements.JAR))
                     it.attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, project.objects.named(TargetJvmEnvironment.class, TargetJvmEnvironment.STANDARD_JVM))
                 }
