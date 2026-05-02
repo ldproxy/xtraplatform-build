139756745abbc3b5bc2410ca0d37fc8c73e652a3
Andreas Zahnen <zahnen@interactive-instruments.de>
2026-05-02 12:59:01 +0200

    spotless cleanthat

--- src/main/groovy/de/interactive_instruments/xtraplatform/LayerPlugin.groovy
+++ src/main/groovy/de/interactive_instruments/xtraplatform/LayerPlugin.groovy
@@ -185,7 +185,11 @@ class LayerPlugin implements Plugin<Project> {
                         targetExclude '**/build/generated/**/*'
 
                         forbidWildcardImports()
-                        cleanthat().addMutator('SafeAndConsensual').addMutator('SafeButNotConsensual').excludeMutator('AvoidInlineConditionals')
+                        cleanthat()
+                                .addMutator('SafeAndConsensual')
+                                .addMutator('SafeButNotConsensual')
+                                .excludeMutator('AvoidInlineConditionals')
+                                .excludeMutator('RemoveExplicitCallToSuper')
                         googleJavaFormat('1.35.0')
 
                         // make sure every file has the following copyright header.
