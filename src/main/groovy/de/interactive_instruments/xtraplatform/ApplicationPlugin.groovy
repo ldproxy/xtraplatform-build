32ed59543e3316e9b8a03a77f74d803e2a55f5e4
Andreas Zahnen <zahnen@interactive-instruments.de>
2026-05-10 09:10:54 +0200

    move jvm args to app

--- src/main/groovy/de/interactive_instruments/xtraplatform/ApplicationPlugin.groovy
+++ src/main/groovy/de/interactive_instruments/xtraplatform/ApplicationPlugin.groovy
@@ -33,7 +33,7 @@ class ApplicationPlugin implements Plugin<Project> {
         ModuleInfoExtension moduleInfo = project.extensions.create('moduleInfo', ModuleInfoExtension)
 
         //suppress java 9+ illegal access warnings for felix, jackson afterburner, geotools/hsqldb, mustache
-        project.application.applicationDefaultJvmArgs += ['--add-opens', 'java.base/java.lang=ALL-UNNAMED', '--add-opens', 'java.base/java.net=ALL-UNNAMED', '--add-opens', 'java.base/java.security=ALL-UNNAMED', '--add-opens', 'java.base/java.nio=ALL-UNNAMED', '--add-opens', 'java.base/java.util=ALL-UNNAMED']
+        //project.application.applicationDefaultJvmArgs += ['--add-opens', 'java.base/java.lang=ALL-UNNAMED', '--add-opens', 'java.base/java.net=ALL-UNNAMED', '--add-opens', 'java.base/java.security=ALL-UNNAMED', '--add-opens', 'java.base/java.nio=ALL-UNNAMED', '--add-opens', 'java.base/java.util=ALL-UNNAMED']
 
         project.configurations.create("featureDevOnly")
         project.configurations.featureDevOnly.resolutionStrategy.cacheDynamicVersionsFor(5, 'minutes')
